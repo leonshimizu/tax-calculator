f48269f5ef99161e8f168e3a02886dfbab0e044a
Leon Shimizu <lmshimizu@gmail.com>
2024-08-17 01:25:02 +1000

    Set the payroll page as the home page, update the calculate page to use the currency function, and switch the places of the page links in the header

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,6 @@ Rails.application.routes.draw do
   get 'calculate', to: 'tax_calculator#calculate'
   get 'payroll/show', to: 'payroll#show'
 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # Set the root to the payroll show action
+  root 'payroll#show'
 end

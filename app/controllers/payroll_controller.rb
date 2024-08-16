973edcd597d6bbd3be8a20b88c9070ad2c9a3ca8
Leon Shimizu <lmshimizu@gmail.com>
2024-08-17 02:31:50 +1000

    Add insurance as one of the parameters and fixed the parameters to default to 0 in case the user doesn't input anything

--- app/controllers/payroll_controller.rb
+++ app/controllers/payroll_controller.rb
@@ -1,11 +1,12 @@
 class PayrollController < ApplicationController
   def show
     if params[:calculate]
-      @total_hours = params[:total_hours].to_f
-      @pay_rate = params[:pay_rate].to_f
-      @reported_tips = params[:reported_tips].to_f
-      @loan = params[:loan].to_f
-      @retirement_rate = params[:retirement_rate].to_f / 100
+      @total_hours = params[:total_hours].to_f || 0
+      @pay_rate = params[:pay_rate].to_f || 0
+      @reported_tips = params[:reported_tips].to_f || 0
+      @loan = params[:loan].to_f || 0
+      @retirement_rate = params[:retirement_rate].to_f / 100 || 0
+      @insurance = params[:insurance].to_f || 0
       @filing_status = params[:filing_status]
 
       @gross_income = (@total_hours * @pay_rate) + @reported_tips
@@ -13,7 +14,7 @@ class PayrollController < ApplicationController
       @social_security_tax = Calculator.calculate_social_security(@gross_income)
       @medicare_tax = Calculator.calculate_medicare(@gross_income)
       @retirement_amount = @gross_income * @retirement_rate
-      @net_pay = @gross_income - (@withholding_tax + @social_security_tax + @medicare_tax + @loan + @retirement_amount)
+      @net_pay = @gross_income - (@withholding_tax + @social_security_tax + @medicare_tax + @loan + @insurance + @retirement_amount)
     end
   end
 end

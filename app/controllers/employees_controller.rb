be8aa8ac5288486d1172258b8002a69f9830bc3d
“leonshimizu” <lmshimizu@gmail.com>
2024-08-18 01:50:40 +1000

    Start adding controller CRUD methods

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,30 @@
 class EmployeesController < ApplicationController
+  def index
+    @employees = Employee.all
+  end
+
+  def show
+    @employee = Employee.find(params[:id])
+  end
+
+  # incomplete - still need to add the data to what's created
+  def new
+    @employee = Employee.new
+  end
+
+  # incomplete - still need to add the data to what's edited
+  def edit
+    @employee = Employee.find(params[:id])
+  end
+
+  def destroy
+    @employee = Employee.find(params[:id])
+    @employee.destroy
+    # redirect_to employees_url, notice: 'Employee was successfully destroyed.'
+  end
+  
+  private
+  def employee_params
+    params.require(:employee).permit(:name, :filing_status, :pay_rate, :401k_rate, :position)
+  end
 end
--- app/controllers/payroll_records_controller.rb
+++ app/controllers/payroll_records_controller.rb
@@ -1,2 +1,28 @@
 class PayrollRecordsController < ApplicationController
+  def create
+    @employee = Employee.find(params[:employee_id])
+    @payroll_record = @employee.payroll_records.new(payroll_record_params)
+    if @payroll_record.save
+      @payroll_record.calculate_payroll # Assuming this method handles all calculations
+      redirect_to employee_path(@employee), notice: 'Payroll record was successfully created and calculated.'
+    else
+      render 'employees/show'
+    end
+  end
+
+  def update
+    @payroll_record = PayrollRecord.find(params[:id])
+    if @payroll_record.update(payroll_record_params)
+      @payroll_record.calculate_payroll # Recalculate payroll after update
+      redirect_to employee_path(@payroll_record.employee), notice: 'Payroll record was successfully updated and recalculated.'
+    else
+      render 'edit'
+    end
+  end
+
+  private
+  def payroll_record_params
+    params.require(:payroll_record).permit(:hours_worked, :overtime_hours_worked, :reported_tips, :loan_payment, :insurance_payment, :date)
+  end
+
 end

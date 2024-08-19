e4b550f1b2a08336383dbdf2792d22a30c4b43d5
Leon Shimizu <lmshimizu@gmail.com>
2024-08-20 03:40:27 +1000

    Delete unused or unneeded code

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,18 +1,7 @@
 # app/models/employee.rb
 class Employee < ApplicationRecord
   has_many :payroll_records, dependent: :destroy
-  before_save :convert_retirement_rate_to_percentage
   validates :retirement_rate, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100, allow_nil: true }
   validates :name, presence: true
   validates :pay_rate, presence: true, numericality: { greater_than_or_equal_to: 0.01 }
-
-  FILING_STATUSES = ['single', 'married', 'head_of_household']
-  POSITIONS = ['front_of_house', 'back_of_house']
-
-  private
-
-  def convert_retirement_rate_to_percentage
-    # Assuming the user inputs a whole number for the percentage
-    self.retirement_rate = retirement_rate / 100.0 unless retirement_rate.blank?
-  end
 end

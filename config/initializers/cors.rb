c6aa66499f8e8d8997ff0d1f84939d8021da0500
Leon Shimizu <lmshimizu@gmail.com>
2024-08-21 02:21:06 +1000

    Allow for other methods through cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,8 +1,9 @@
+# config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins '*'  # For testing only, specify domains for production
     resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :delete, :options]
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end

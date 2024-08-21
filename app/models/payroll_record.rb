75919abee1c51bd072151aa759e4e431ddd5d3aa
Leon Shimizu <lmshimizu@gmail.com>
2024-08-22 01:51:21 +1000

    Remove redundant logic and update calculations to be rounded to 2 decimal places

--- app/models/payroll_record.rb
+++ app/models/payroll_record.rb
@@ -11,15 +11,15 @@ class PayrollRecord < ApplicationRecord
 
   def calculate_gross_pay
     overtime_pay = self.overtime_hours_worked.to_f * employee.pay_rate * 1.5
-    self.gross_pay = (self.hours_worked * employee.pay_rate) + overtime_pay + self.reported_tips.to_f
+    self.gross_pay = ((self.hours_worked * employee.pay_rate) + overtime_pay + self.reported_tips.to_f).round(2)
   end
 
   def update_payroll_details
     # Calculate all payroll details
     calculate_gross_pay
-    self.withholding_tax = Calculator.calculate_withholding(self.gross_pay, employee.filing_status)
-    self.social_security_tax = Calculator.calculate_social_security(self.gross_pay)
-    self.medicare_tax = Calculator.calculate_medicare(self.gross_pay)
+    calculate_withholding
+    calculate_social_security
+    calculate_medicare
     calculate_retirement_payment
     calculate_net_pay
   end
@@ -29,22 +29,22 @@ class PayrollRecord < ApplicationRecord
     fields = [self.withholding_tax, self.social_security_tax, self.medicare_tax, self.loan_payment, self.insurance_payment, self.retirement_payment]
     total_deductions = fields.map(&:to_f).sum
   
-    self.net_pay = self.gross_pay.to_f - total_deductions
+    self.net_pay = (self.gross_pay.to_f - total_deductions).round(2)
   end
 
   def calculate_withholding
-    self.withholding_tax = Calculator.calculate_withholding(self.gross_pay, employee.filing_status)
+    self.withholding_tax = Calculator.calculate_withholding(self.gross_pay, employee.filing_status).round(2)
   end
 
   def calculate_social_security
-    self.social_security_tax = Calculator.calculate_social_security(self.gross_pay)
+    self.social_security_tax = Calculator.calculate_social_security(self.gross_pay).round(2)
   end
 
   def calculate_medicare
-    self.medicare_tax = Calculator.calculate_medicare(self.gross_pay)
+    self.medicare_tax = Calculator.calculate_medicare(self.gross_pay).round(2)
   end
 
   def calculate_retirement_payment
-    self.retirement_payment = self.gross_pay.to_f * (employee.retirement_rate.to_f / 100)
+    self.retirement_payment = (self.gross_pay.to_f * (employee.retirement_rate.to_f / 100)).round(2)
   end
 end

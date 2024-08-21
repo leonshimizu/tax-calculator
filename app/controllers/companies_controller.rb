77e44a48cc75f68d8a936186a40e1171e83620fb
Leon Shimizu <lmshimizu@gmail.com>
2024-08-22 05:58:09 +1000

    Update companies controller to have the other crud actions

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,13 +1,41 @@
 # app/controllers/companies_controller.rb
 class CompaniesController < ApplicationController
+  before_action :set_company, only: [:show, :update, :destroy]
+
+  # GET /companies
   def index
     @companies = Company.all
     render json: @companies
   end
 
+  # GET /companies/:id
   def show
-    @company = Company.find(params[:id])
-    render json: @company.as_json(include: :employees)
+    render json: @company
+  end
+
+  # POST /companies
+  def create
+    @company = Company.new(company_params)
+
+    if @company.save
+      render json: @company, status: :created
+    else
+      render json: @company.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /companies/:id
+  def update
+    if @company.update(company_params)
+      render json: @company
+    else
+      render json: @company.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /companies/:id
+  def destroy
+    @company.destroy
   end
 
   def department_ytd_totals
@@ -21,5 +49,15 @@ class CompaniesController < ApplicationController
     company = Company.find(params[:id])
     year = params[:year].to_i || Time.current.year
     render json: company.company_ytd_totals(year)
+  end
+
+  private
+
+  def set_company
+    @company = Company.find(params[:id])
+  end
+
+  def company_params
+    params.require(:company).permit(:name, :address, :phone_number, :email) # Adjust the permitted parameters as needed
   end
 end

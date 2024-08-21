454bba0bad2c90cc65c5deef25860fb367e14fa7
Leon Shimizu <lmshimizu@gmail.com>
2024-08-21 15:22:31 +1000

    Update the routes and payroll records controller to accept batch payroll record create and index requests

--- app/controllers/payroll_records_controller.rb
+++ app/controllers/payroll_records_controller.rb
@@ -1,12 +1,17 @@
 # app/controllers/payroll_records_controller.rb
 class PayrollRecordsController < ApplicationController
-  before_action :set_employee
+  before_action :set_employee, only: [:index, :create]
   before_action :set_payroll_record, only: [:show, :update, :destroy]
   rescue_from ActiveRecord::RecordNotFound, with: :handle_not_found
 
   # GET /employees/:employee_id/payroll_records
   def index
-    @payroll_records = @employee.payroll_records.all
+    if params[:date].present?
+      @payroll_records = PayrollRecord.where(date: params[:date])
+    else
+      @payroll_records = @employee.payroll_records.all
+    end
+
     render json: @payroll_records
   end
 
@@ -23,12 +28,34 @@ class PayrollRecordsController < ApplicationController
     }
   end
 
+  # POST /employees/:employee_id/payroll_records
   def create
     @payroll_record = @employee.payroll_records.build(payroll_record_params)
     if @payroll_record.save
       render json: @payroll_record, status: :created, location: employee_payroll_record_path(@employee, @payroll_record)
     else
       render json: @payroll_record.errors, status: :unprocessable_entity
+    end
+  end
+
+  # POST /employees/batch/payroll_records
+  def batch_create
+    payroll_records_params = params.require(:payroll_records).map do |record|
+      record.permit(:employee_id, :date, :hours_worked, :overtime_hours_worked, :reported_tips, :loan_payment, :insurance_payment)
+    end
+
+    created_records = payroll_records_params.map do |record_params|
+      employee = Employee.find_by(id: record_params[:employee_id])
+      unless employee
+        render json: { error: "Employee with ID #{record_params[:employee_id]} not found." }, status: :not_found and return
+      end
+      employee.payroll_records.create(record_params)
+    end
+    
+    if created_records.all?(&:persisted?)
+      render json: created_records, status: :created
+    else
+      render json: { errors: created_records.map(&:errors) }, status: :unprocessable_entity
     end
   end
 
@@ -54,7 +81,9 @@ class PayrollRecordsController < ApplicationController
 
   # Use callbacks to share common setup or constraints between actions
   def set_employee
-    @employee = Employee.find(params[:employee_id])
+    if params[:employee_id]
+      @employee = Employee.find(params[:employee_id])
+    end
   end
 
   def set_payroll_record
@@ -67,15 +96,6 @@ class PayrollRecordsController < ApplicationController
 
   # Only allow a list of trusted parameters through
   def payroll_record_params
-    params.require(:payroll_record).permit(:hours_worked, :overtime_hours_worked, :reported_tips, :loan_payment, :insurance_payment, :retirement_payment, :date)
+    params.require(:payroll_record).permit(:hours_worked, :overtime_hours_worked, :reported_tips, :loan_payment, :insurance_payment, :date)
   end
-
-  # method isn't currently being used but I'd like to keep it here till I verify that won't use it in the future
-  # def calculate_payroll_details(payroll_record)
-  #   gross_pay = payroll_record.calculate_gross_pay # Assumes a method in PayrollRecord
-  #   payroll_record.withholding_tax = Calculator.calculate_withholding(gross_pay, payroll_record.employee.filing_status)
-  #   payroll_record.social_security_tax = Calculator.calculate_social_security(gross_pay)
-  #   payroll_record.medicare_tax = Calculator.calculate_medicare(gross_pay)
-  #   payroll_record.net_pay = Calculator.calculate_net_pay(gross_pay, payroll_record.withholding_tax, payroll_record.social_security_tax, payroll_record.medicare_tax)
-  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
+# config/routes.rb
 Rails.application.routes.draw do
+  # Define a separate route for batch processing
+  post 'employees/batch/payroll_records', to: 'payroll_records#batch_create'
+  get 'payroll_records', to: 'payroll_records#index'
+  
+  # Other routes for individual payroll records
+  resources :employees do
+    resources :payroll_records, only: [:index, :show, :create, :update, :destroy]
+  end
+  
   get 'calculate', to: 'tax_calculator#calculate'
   get 'net_pay/show', to: 'net_pay#show'
-
-  # Set the root to the payroll show action
   root 'net_pay#show'
-
-  resources :employees do
-    resources :payroll_records
-  end
 end
+

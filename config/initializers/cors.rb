bc91d7e985e76e2a11d1a62fccf90d4ebe10740d
“leonshimizu” <lmshimizu@gmail.com>
2024-08-27 15:50:33 +1000

    Configure cors to only receive requests from http://localhost:3000 and https://tax-calculator-frontend-2-0.onrender.com

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,9 +1,11 @@
 # config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'  # For testing only, specify domains for production
+    origins 'http://localhost:3000', 'https://tax-calculator-frontend-2-0.onrender.com'
+
     resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: true
   end
 end

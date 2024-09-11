2522a479a23d7af471ee054a3e273d501a24546d
Leon Shimizu <lmshimizu@gmail.com>
2024-09-11 22:24:30 +1000

    Allow the backend to accept requests from the netlify frontend

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 # config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000', 'http://localhost:3001', 'https://tax-calculator-frontend-2-0.onrender.com'
+    origins 'http://localhost:3000', 'http://localhost:3001', 'https://tax-calculator-frontend-2-0.onrender.com', 'https://tax-calculator-frontend.netlify.app/'
 
     resource '*',
       headers: :any,
